3e3667f39f7e78c908159e9128a693130979f844
Elna Bittaj <elnabittaj@gmail.com>
2025-07-30 10:46:31 +0530

    Create Week.java

---
+++ Week.java
@@ -0,0 +1,36 @@
+package pro3;
+import java.util.Scanner;
+public class week {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter a number from 1 to 7: ");
+		int day=sc.nextInt();
+		switch (day) {
+		case 1:
+			System.out.println("Day: Sunday");
+			break;
+		case 2:
+			System.out.println("Day: Monday");
+			break;
+		case 3:
+			System.out.println("Day: Tuesday");
+			break;
+		case 4:
+			System.out.println("Day: Wednesday");
+			break;
+		case 5:
+			System.out.println("Day: Thursday");
+			break;
+		case 6:
+			System.out.println("Day: Friday");
+			break;
+		case 7:
+			System.out.println("Day: Saturday");
+			break;
+		default:
+			System.out.println("Invalid Entry!");
+			break;
+		}
+	}
+
+}
